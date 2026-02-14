4d330874b12324b2c3e3c9a608b80dd920b1aab1
MARVEL <parth18174@gmail.com>
2026-02-14 21:57:10 +0530

    implemented a approach to mitigate bottom pipe

--- main.cpp
+++ main.cpp
@@ -35,13 +35,15 @@ int main()
     std::vector<Pipe> pipes;
     const float pipeSpeed = 200.0f;
     const int pipeWidth = 60;
-    const int gap = 150;
+    const int gap = 165;
 
     float pipeTimer = 0;
     int score = 0;
     bool gameOver = false;
 
-    Texture2D birdTex = LoadTexture("sprite_sheet.png");
+    Texture2D birdTex = LoadTexture("opSprite.png");
+    Texture2D pipeTex = LoadTexture("pipe.png");
+    Texture2D pipetopTex = LoadTexture("top_pipe.png");
     const int frameCount = 3; // number of frames in sprite sheet
     const int frameWidth = birdTex.width / frameCount;
     const int frameHeight = birdTex.height;
@@ -64,16 +66,16 @@ int main()
             }
 
             // Gravity
-            bird.velocity += gravity * dt;
+            bird.velocity += gravity * dt; // 1s = 15 pixel drop per frame
             bird.rect.y += bird.velocity * dt;
 
             // Pipe spawn
             pipeTimer += dt;
-            if (pipeTimer > 1.5f)
+            if (pipeTimer > 1.5f) // every 1.5 seconds pipe will be spawn
             {
                 pipeTimer = 0;
 
-                int topHeight = GetRandomValue(50, 300);
+                int topHeight = GetRandomValue(50, 230); // this is top pipe height
                 Pipe p;
                 p.top = {(float)screenWidth, 0, (float)pipeWidth, (float)topHeight};
                 p.bottom = {(float)screenWidth, (float)(topHeight + gap),
@@ -144,11 +146,11 @@ int main()
         // DrawRectangleRec(bird.rect, YELLOW);
 
         // Pipes
-        for (auto &p : pipes)
-        {
-            DrawRectangleRec(p.top, GREEN);
-            DrawRectangleRec(p.bottom, GREEN);
-        }
+        // // for (auto &p : pipes)
+        // {
+        //     DrawRectangleRec(p.top, GREEN);
+        //     DrawRectangleRec(p.bottom, GREEN);
+        // }
 
         // Score
         DrawText(TextFormat("Score: %d", score), 20, 20, 20, BLACK);
@@ -179,9 +181,140 @@ int main()
             0.0f,
             WHITE);
 
+        // now pipe texture
+        for (auto &p : pipes)
+        {
+            float neededHeight = p.bottom.height;
+
+            // ---- If pipe height <= texture height ----
+            if (neededHeight <= pipeTex.height)
+            {
+                Rectangle pipe_source = {
+                    0.0f,
+                    0.0f,
+                    (float)pipeTex.width,
+                    neededHeight};
+
+                Rectangle pipe_dest = {
+                    p.bottom.x,
+                    p.bottom.y,
+                    p.bottom.width,
+                    neededHeight};
+
+                DrawTexturePro(pipeTex,
+                               pipe_source,
+                               pipe_dest,
+                               {0, 0},
+                               0.0f,
+                               WHITE);
+            }
+            else
+            {
+                // ---- If pipe height is larger than texture ----
+                // Draw full texture stretched to pipe height
+
+                Rectangle pipe_source = {
+                    0.0f,
+                    0.0f,
+                    (float)pipeTex.width,
+                    (float)pipeTex.height};
+
+                Rectangle pipe_dest = {
+                    p.bottom.x,
+                    p.bottom.y,
+                    p.bottom.width,
+                    p.bottom.height};
+
+                DrawTexturePro(pipeTex,
+                               pipe_source,
+                               pipe_dest,
+                               {0, 0},
+                               0.0f,
+                               WHITE);
+            }
+        }
+
+        // ---- TOP PIPE ----
+        for (auto &p : pipes)
+        {
+            float neededHeight = p.top.height;
+
+            if (neededHeight <= pipeTex.height)
+            {
+                // Slice from top downward (include cap)
+                Rectangle pipe_source = {
+                    0.0f,
+                    0.0f,
+                    (float)pipeTex.width,
+                    neededHeight};
+
+                // Flip vertically by using negative height
+                Rectangle pipe_dest = {
+                    p.top.x,
+                    p.top.y + neededHeight, // move down to gap edge
+                    p.top.width,
+                    -neededHeight // grow upward
+                };
+
+                DrawTexturePro(pipeTex,
+                               pipe_source,
+                               pipe_dest,
+                               {0, 0},
+                               0.0f,
+                               WHITE);
+            }
+            else
+            {
+                // If pipe taller than texture
+                Rectangle pipe_source = {
+                    0.0f,
+                    0.0f,
+                    (float)pipeTex.width,
+                    (float)pipeTex.height};
+
+                Rectangle pipe_dest = {
+                    p.top.x,
+                    p.top.y + p.top.height,
+                    p.top.width,
+                    -p.top.height};
+
+                DrawTexturePro(pipeTex,
+                               pipe_source,
+                               pipe_dest,
+                               {0, 0},
+                               0.0f,
+                               WHITE);
+            }
+        }
+
+        //     for (auto &p : pipes)
+        //   {
+        /////////////////////////////////////////////////
+        // Rectangle flipped_source = {
+        //     0,
+        //     (float)pipeTex.height,
+        //     (float)pipeTex.width,
+        //     -(float)pipeTex.height};
+
+        // Rectangle pipe_dest_top = {
+        //     p.top.x,
+        //     p.top.y + p.top.height,
+        //     p.top.width,
+        //     p.top.height,
+        // };
+
+        // DrawTexturePro(pipeTex,
+        //                flipped_source,
+        //                pipe_dest_top,
+        //                {0, 0},
+        //                0.0f,
+        //                WHITE);
+        //       }
         EndDrawing();
     }
     UnloadTexture(birdTex);
+    UnloadTexture(pipeTex);
+    UnloadTexture(pipetopTex);
     CloseWindow();
     return 0;
 }
